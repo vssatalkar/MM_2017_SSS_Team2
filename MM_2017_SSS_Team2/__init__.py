c98c0e1bbc183a3b90db7bed52e3a76b8961fb7d
Viswanath Pasumarthi <pasumart@buffalo.edu>
2017-07-26 17:23:01 -0400

    avoid importing math to __init__

--- MM_2017_SSS_Team2/__init__.py
+++ MM_2017_SSS_Team2/__init__.py
@@ -2,6 +2,6 @@
 This is the base file of the effective-fortnight!
 """
 from . import minimum_image_distance
-from . import math
-from .math import mult
-from .math import power
+#from . import math
+#from .math import mult
+#from .math import power
