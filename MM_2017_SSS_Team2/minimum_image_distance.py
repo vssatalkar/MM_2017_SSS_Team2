ed3dae7996725c9b332b52c5c0e24f5f9dfbd737
Viswanath Pasumarthi <pasumart@buffalo.edu>
2017-07-26 17:15:29 -0400

    add lj_potential to mid module

--- MM_2017_SSS_Team2/minimum_image_distance.py
+++ MM_2017_SSS_Team2/minimum_image_distance.py
@@ -8,3 +8,13 @@ def minimum_image_distance(r_i, r_j, box_length):
     rij -= box_length * np.round(rij / box_length)
     rij2 = np.dot(rij, rij)
     return rij2
+
+def lennard_jones_potential(rij2):
+    """
+    Function to compute Lennard Jones potential for a given squared distance 
+    """
+    sig_by_r6 = (1 / rij2)**3
+    sig_by_r12 = sig_by_r6**2
+    potential_energy = 4.0 * (sig_by_r12 - sig_by_r6)
+    return potential_energy
+
